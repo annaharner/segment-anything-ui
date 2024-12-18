e4832f48e9fee909ded7842e62fe249239e3930c
Soren Harner <soren@layerjot.com>
2024-12-17 16:08:49 -0800

    Remove broken dependency

--- segment_anything_ui/model_builder.py
+++ segment_anything_ui/model_builder.py
@@ -1,5 +1,5 @@
 try:
-    from efficientvit.sam_model_zoo import create_sam_model, EfficientViTSam
+    from efficientvit.sam_model_zoo import EfficientViTSam
     from efficientvit.models.efficientvit.sam import EfficientViTSamPredictor, EfficientViTSamAutomaticMaskGenerator
     IS_EFFICIENT_VIT_AVAILABLE = True
 except (ModuleNotFoundError, ImportError) as e:
@@ -14,11 +14,11 @@ from segment_anything.build_sam import Sam
 
 def build_model(model_name: str, checkpoint_path: str, device: str):
     match model_name:
-        case "xl0" | "xl1":
-            efficientvit_sam = create_sam_model(
-                name=model_name, weight_url=checkpoint_path,
-            )
-            return efficientvit_sam.to(device).eval()
+        # case "xl0" | "xl1":
+        #     efficientvit_sam = create_sam_model(
+        #         name=model_name, weight_url=checkpoint_path,
+        #     )
+        #     return efficientvit_sam.to(device).eval()
         
         case "vit_b" | "vit_l" | "vit_h":
             sam = sam_model_registry[model_name](
